4d2d4a64bf80ba80f90e94d57884bcad1c6244c9
achukash <acchute.kashyap@gmail.com>
2024-09-15 05:53:08 +0530

    chore: Remove unused code and update user_stats.py

--- user_stats.py
+++ user_stats.py
@@ -10,10 +10,7 @@ def get_user_stats(username):
         if "title" in all_stats:
             wanted_stats["title"] = all_stats["title"]
         wanted_stats["username"] = all_stats["username"]
-        if "profile" in all_stats:  
-            print("came in")
-            wanted_stats["flag_url"] = "https://flagsapi.com/" + all_stats["profile"]["flag"] + "/flat/64.png"
-            
+             
         for ratings in all_stats["perfs"]:
             if ratings in ["bullet", "blitz", "rapid", "classical"]:
                 wanted_stats[f"{ratings}_rating"] = all_stats["perfs"][ratings]["rating"]
